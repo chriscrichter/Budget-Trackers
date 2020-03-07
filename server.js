71936665b985563655f1b54fcf1d4273acdb6b9a
chriscrichter <chris_richter@outlook.com>
2020-03-07 10:32:51 -0500

    Updated server.js file.

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -22,7 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://chris:coder2020@ds215988.
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
