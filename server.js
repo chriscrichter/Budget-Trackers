aef1a3bbeab6be9d1b2f2d149c15ed062f8f2537
chriscrichter <chris_richter@outlook.com>
2020-03-07 10:20:31 -0500

    Added MongoDB info.

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+// mongodb://chris:coder2020@ds215988.mlab.com:15988/heroku_nb8h8m0r
+// mongodb://localhost/budget
+mongoose.connect(process.env.MONGODB_URI || "mongodb://chris:coder2020@ds215988.mlab.com:15988/heroku_nb8h8m0r", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
